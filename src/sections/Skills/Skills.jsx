2a2d189bfc0edadeac836d9155a1065d7f75fbfd
Jameshuff95 <126839048+Jameshuff95@users.noreply.github.com>
2024-08-28 22:40:55 -0400

    re-arranged skills

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -13,34 +13,33 @@ function Skills() {
       <h1 className="sectionTitle">Skills</h1>
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="Google Sheets" />
+        <SkillList src={checkMarkIcon} skill="Microsoft Excel" />
         <SkillList src={checkMarkIcon} skill="Microsoft Powerpoint" />
         <SkillList src={checkMarkIcon} skill="Microsoft Word" />
-        <SkillList src={checkMarkIcon} skill="Microsoft Excel" />
       </div>
       <hr />
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="HTML" />
-        <SkillList src={checkMarkIcon} skill="CSS" />
         <SkillList src={checkMarkIcon} skill="Bootstrap CSS" />
+        <SkillList src={checkMarkIcon} skill="CSS" />
+        <SkillList src={checkMarkIcon} skill="HTML" />
         <SkillList src={checkMarkIcon} skill="SCSS" />
       </div>
       <hr />
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="Javascript" />
+        <SkillList src={checkMarkIcon} skill="Node.js" />
         <SkillList src={checkMarkIcon} skill="React.js" />
         <SkillList src={checkMarkIcon} skill="Vite.js" />
-        <SkillList src={checkMarkIcon} skill="Node.js" />
       </div>
       <hr />
       <div className={styles.skillList}>
-      <SkillList src={checkMarkIcon} skill="Python" />
-        <SkillList src={checkMarkIcon} skill="Pygame" />
-        <SkillList src={checkMarkIcon} skill="Django" />
         <SkillList src={checkMarkIcon} skill="C++" />
-        <SkillList src={checkMarkIcon} skill="MySQL" />
-        <SkillList src={checkMarkIcon} skill="MariaDB" />
+        <SkillList src={checkMarkIcon} skill="Django" />
         <SkillList src={checkMarkIcon} skill="PostgreSQL" />
-        <SkillList src={checkMarkIcon} skill="Windows Powershell" />
+        <SkillList src={checkMarkIcon} skill="Python" />
+        <SkillList src={checkMarkIcon} skill="Pygame" />
+        <SkillList src={checkMarkIcon} skill="MariaDB" />
+        <SkillList src={checkMarkIcon} skill="MySQL" />
       </div>
       <hr />
       <div className={styles.skillList}>
@@ -50,6 +49,7 @@ function Skills() {
         <SkillList src={checkMarkIcon} skill="Github" />
         <SkillList src={checkMarkIcon} skill="Netlify" />
         <SkillList src={checkMarkIcon} skill="Unreal Game Engine" />
+        <SkillList src={checkMarkIcon} skill="Windows Powershell" />
         <SkillList src={checkMarkIcon} skill="XAMPP" />
       </div>
       <hr />
